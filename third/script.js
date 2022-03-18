5a89f95d400e9fa94d1219afd4a9ddc7b923ca36
whit33y <troppout@gmail.com>
2022-03-18 09:48:06 +0100

    No duplicates etc

---
+++ third/script.js
@@ -0,0 +1,62 @@
+function mathRandom(){
+return Math.ceil(Math.random()*49)
+}
+let losowe = []
+for(let i=0; i<6; i++){
+x = mathRandom();
+if(losowe.includes(x)){i--}
+else{losowe.push(x)}
+}
+console.log(losowe)
+let form = document.getElementById('form')
+function check(){
+    list = []
+    for(let i=0; i<6; i++){
+      list[i] = document.getElementById('number'+i).value
+    }
+    for (i = 0; i < 6; i++) {
+        for (j = i + 1; j < 6; j++) {
+            if (i == j || list[i] == "" || list[j] == "") 
+             continue;
+         if (list[i] == list[j]) {
+         document.getElementById("status" + i)
+           .innerHTML = "Duplicated values, your ticket is invalid!";
+           list.push('false')
+         document.getElementById("status" + j)
+          .innerHTML = "Duplicated values, your ticket is invalid!";
+              }
+          
+          }   
+      }
+      trafione = 0
+      if(list.length === 6){
+          for(let i=0; i<6; i++){
+              list[i] = parseInt(list[i])
+          }
+          for(let j=0; j<6; j++){
+              for(let k=0;k<6;k++){
+                  if(list[j]===losowe[k]){
+                      trafione++
+                  }
+              }
+          }
+      }
+    // list.pop()
+    // list.pop()
+    // list.pop()
+    // list.push(parseInt(number0.value), parseInt(number1.value), parseInt(number2.value))
+    // document.getElementById('shots').innerHTML = losowe
+    if(list.length === 6){
+    document.getElementById('yourShots').innerHTML = list
+    document.getElementById('goodShots').innerHTML = trafione
+    for(let i=0; i<6; i++){
+        document.getElementById("status" + i)
+           .innerHTML = "";
+    }
+    }
+}
+form.addEventListener('submit',function(event){
+    event.preventDefault()
+    check()
+    
+})
